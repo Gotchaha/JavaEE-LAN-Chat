f2947e3fe2f848edd61886b1b4de1af8e007ae70
wangenming <253986041@qq.com>
2020-06-17 11:32:53 +0800

    dao

--- src/com/LANChat/dao/IUserDao.java
+++ src/com/LANChat/dao/IUserDao.java
@@ -1,4 +1,17 @@
 package com.LANChat.dao;
 
+import com.LANChat.entity.NhManageUser;
+
+import java.util.List;
+
 public interface IUserDao {
+    List<NhManageUser> findUserByCondition(NhManageUser user);
+
+    int findCountByCondition(NhManageUser user);
+
+    NhManageUser findByID(int id);
+
+    void update(NhManageUser user);
+
+    void insert(NhManageUser user);
 }
--- src/com/LANChat/util/ConnUtil.java
+++ src/com/LANChat/util/ConnUtil.java
@@ -1,23 +1,42 @@
 package com.LANChat.util;
 
+import com.alibaba.druid.pool.DruidDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnUtil {
 
+    public static DataSource getDataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/tm");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        return  dataSource;
+    }
+
+
     public  static Connection getConn(){
-        try {
-            Class.forName("com.mysql.jc.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hhit","root","root");
+            conn = getDataSource().getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return conn;
+    }
+
+    public static void close(Connection conn){
+        if(conn!=null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
