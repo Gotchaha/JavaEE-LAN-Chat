44347d19a7bf1bc9f3d6fac8641b5c0aaca036d7
Lu Yiming <luyiming.2009@163.com>
2020-06-17 15:41:06 +0800

    聊天记录Message类与相关存储聊天记录的servlet实现 text文档更新（git注意点）

--- src/com/LANChat/action/ServletSay.java
+++ src/com/LANChat/action/ServletSay.java
@@ -1,15 +1,24 @@
 package com.LANChat.action;
 
+import com.LANChat.entity.Message;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @WebServlet(name = "ServletSay",urlPatterns = "/ServletSay")
+// 实现将聊天记录保存到数据库中
+
 public class ServletSay extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String msg = request.getParameter("message");
+        Message message = new Message(null, msg, new Date());
+        System.out.println(message);
 
     }
 
